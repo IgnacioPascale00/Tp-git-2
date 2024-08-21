c6ef686ed1410b3bc0ded9acfe75cb736b7221a7
IgnacioPascale00 <nachipascale@gmail.com>
2024-08-21 17:46:13 -0300

    SegundoCommit

--- src/calculadora/interfaz/Calculadora.java
+++ src/calculadora/interfaz/Calculadora.java
@@ -10,7 +10,7 @@ public class Calculadora {
 		Integer a;
 		Integer b = null;
 		Integer opcion;
-		Integer resultado;
+		Integer resultado = null;
 
 		do {
 			System.out.println(
@@ -21,14 +21,15 @@ public class Calculadora {
 		a = teclado.nextInt();
 		System.out.println("Ingrese el segundo numero");
 		a = teclado.nextInt();
-		if (opcion.equals(1)) {
-			resultado = a + b;
-		} else if (opcion.equals(2)) {
-			resultado = a - b;
-		} else if (opcion.equals(3)) {
-			resultado = a * b;
-		} else {
-			resultado = a / b;
+		switch (opcion){
+		case 1: resultado = a + b;
+		 break;
+		case 2: resultado = a - b;
+		 break;
+		case 3: resultado = a * b;
+		break;
+		case 4: resultado = a / b;
+		break;
 		}
 		System.out.println("El resultado es " + resultado.toString());
 	}
